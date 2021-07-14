bc35c0210397cbd5ed377c6f7e07fea1d9fbfaa5
김재정 <jaejeong.kim@kbfg.com>
2021-07-14 10:31:33 +0900

    log 확인

--- pororo_api/pororo_api/settings.py
+++ pororo_api/pororo_api/settings.py
@@ -109,8 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'utf-8'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'utf-8'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Seoul'
