d7eefece3fcf2d364ced0a7d63694978e8e4a085
김재정 <jaejeong.kim@kbfg.com>
2021-07-14 18:25:43 +0900

    qa 수정

--- pororo_api/model/pororo_qa.py
+++ pororo_api/model/pororo_qa.py
@@ -3,4 +3,6 @@ from pororo import Pororo
 def func_qa(question, original_news_data):
     mrc = Pororo(task = "mrc", lang = "ko")
 
+    print(mrc(question, original_news_data))
+
     return mrc(question, original_news_data)
