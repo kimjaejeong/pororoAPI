a7bf699c157cee568344dc29247af5c1a2311377
김재정 <jaejeong.kim@kbfg.com>
2021-07-15 12:56:53 +0900

    문구 수정

--- pororo_api/model/pororo_qa.py
+++ pororo_api/model/pororo_qa.py
@@ -5,6 +5,6 @@ def func_qa(question, original_news_data):
 
     answer_sentence = mrc(question, original_news_data)[0]
     if answer_sentence == "":
-        return "AI가 해석하지 못한 질문입니다. 다시 입력 바랍니다."
+        return "[AI 답변]\nAI가 해석하지 못한 질문입니다. 다시 입력 바랍니다."
     else:
         return answer_sentence
